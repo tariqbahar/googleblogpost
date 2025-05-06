3a9ace7b18ffc75061c075137799c44b3bf6ec9a
Tariq Bahar <97286291+tariqbahar@users.noreply.github.com>
2025-05-06 11:10:35 +0430

    video path solved

--- app/videos/page.js
+++ app/videos/page.js
@@ -1,31 +1,31 @@
-// import Header from "@/components/home-3/Header";
-// import LayoutThree from "@/components/home-3/Layout";
-// import { Suspense } from "react";
-// import AllVideos from "./AllVideos";
+import Header from "@/components/home-3/Header";
+import LayoutThree from "@/components/home-3/Layout";
+import { Suspense } from "react";
+import AllVideos from "./AllVideos";
 
-// export const metadata = {
-//   title: "All Video Posts",
-//   description: "All of Eyolo's Video Posts",
-// };
+export const metadata = {
+  title: "All Video Posts",
+  description: "All of Eyolo's Video Posts",
+};
 
-// const Videos = () => {
-//   const postsPerPage = 4;
+const Videos = () => {
+  const postsPerPage = 4;
 
-//   return (
-//     <Suspense
-//       fallback={
-//         <div className="text-xl text-dark font-primary font-bold fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-//           Loading...
-//         </div>
-//       }
-//     >
-//       <LayoutThree>
-//         <Header />
+  return (
+    <Suspense
+      fallback={
+        <div className="text-xl text-dark font-primary font-bold fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          Loading...
+        </div>
+      }
+    >
+      <LayoutThree>
+        <Header />
 
-//         <AllVideos postsPerPage={postsPerPage} />
-//       </LayoutThree>
-//     </Suspense>
-//   );
-// };
+        <AllVideos postsPerPage={postsPerPage} />
+      </LayoutThree>
+    </Suspense>
+  );
+};
 
-// export default Videos;
+export default Videos;
